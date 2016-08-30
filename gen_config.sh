36ff251a16f9b3aba87db21dae8ea697f391c98c
Crixec <79236107@qq.com>
2016-08-30 13:59:49 +0800

    build a single binary likes busybox

--- gen_config.sh
+++ gen_config.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 CROSS_COMPILE="$1"
-./configure --host="${CROSS_COMPILE}" --prefix="$PWD/${CROSS_COMPILE}-coreutils" --enable-no-install-program=stdbuf CFLAGS='-static -O2 -pie' LDFLAGS='-static -O2 -pie'
+./configure --host="${CROSS_COMPILE}" --target="${CROSS_COMPILE}" --prefix="$PWD/${CROSS_COMPILE}-coreutils" --enable-single-binary=symlinks --enable-no-install-program=stdbuf --without-selinux --without-gmp --with-gnu-ld CFLAGS='-static -O2 -fPIE' LDFLAGS='-static -O2 -fPIE
